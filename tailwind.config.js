c642d301fb08a26f72ec822411f1ab542e83f94a
mohdxnizamv <nizam.active@gmail.com>
2022-09-04 07:54:38 +0800

    perubahan fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        Alata: ["Poppins", "sans-serif"],
+        Alata: ["Alata", "sans-serif"],
         screen: {},
       },
     },
