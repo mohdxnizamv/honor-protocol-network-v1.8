2cae0c53f590b5ea39cc2d59744be4d2615fff1d
mohdxnizamv <nizam.active@gmail.com>
2022-09-09 15:24:18 +0800

    add toggle remove icon search and bell

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,44 @@
-//Metamask Connection
-document.getElementById("connect").addEventListener("click", (event) => {
-  let button = event.target;
-  ethereum.request({ method: "eth_requestAccounts" }).then((accounts) => {
-    account = accounts[0];
-    console.log(account);
-    button.textContent = account;
-
-    ethereum
-      .request({ method: "eth_getBalance", params: [account, "latest"] })
-      .then((result) => {
-        console.log(result);
-        let wei = parseInt(result, 16);
-        let balance = wei / 10 ** 18;
-        console.log(balance + " ETH");
+function themeToggle() {
+  var toggleEl = document.querySelector("[data-toggle-theme]");
+  (function (theme = localStorage.getItem("theme")) {
+    if (localStorage.getItem("theme")) {
+      document.documentElement.setAttribute("data-theme", theme);
+      if (toggleEl) {
+        [...document.querySelectorAll("[data-toggle-theme]")].forEach((el) => {
+          el.classList.add(toggleEl.getAttribute("data-act-class"));
+        });
+      }
+    }
+  })();
+  if (toggleEl) {
+    [...document.querySelectorAll("[data-toggle-theme]")].forEach((el) => {
+      el.addEventListener("click", function () {
+        var themesList = el.getAttribute("data-toggle-theme");
+        if (themesList) {
+          var themesArray = themesList.split(",");
+          if (
+            document.documentElement.getAttribute("data-theme") ==
+            themesArray[0]
+          ) {
+            if (themesArray.length == 1) {
+              document.documentElement.removeAttribute("data-theme");
+              localStorage.removeItem("theme");
+            } else {
+              document.documentElement.setAttribute(
+                "data-theme",
+                themesArray[1]
+              );
+              localStorage.setItem("theme", themesArray[1]);
+            }
+          } else {
+            document.documentElement.setAttribute("data-theme", themesArray[0]);
+            localStorage.setItem("theme", themesArray[0]);
+          }
+        }
+        [...document.querySelectorAll("[data-toggle-theme]")].forEach((el) => {
+          el.classList.toggle(this.getAttribute("data-act-class"));
+        });
       });
-  });
-});
+    });
+  }
+}
