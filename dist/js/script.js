ac7c3484598c48e628a7c15812e7279723dfdf90
Mohd Nizamuddin Mad Yusof <58153279+mohdxnizamv@users.noreply.github.com>
2022-10-12 12:38:21 +0800

    first commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,7 @@
 const btn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
-const input = document.getElementById("link-input");
-const linkForm = document.getElementById("link-form");
-const errMsg = document.getElementById("err-msg");
-
 btn.addEventListener("click", navToggle);
-linkForm.addEventListener("submit", formSubmit);
 
 // Toggle Mobile Menu
 function navToggle() {
