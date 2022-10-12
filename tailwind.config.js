f61b094ec236c2b8622ac6cad0c04161b7605448
Mohd Nizamuddin Mad Yusof <58153279+mohdxnizamv@users.noreply.github.com>
2022-10-12 10:00:15 +0800

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,25 @@
 module.exports = {
   content: ["**/*.html"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {
       fontFamily: {
         Alata: ["Alata", "sans-serif"],
-        screen: {},
+        Josefin: ["Josefin Sans", "sans-serif"],
+      },
+      colors: {
+        cyan: "hsl(180, 66%, 49%)",
+        cyanLight: "hsl(180, 66%, 69%)",
+        darkViolet: "hsl(257, 27%, 26%)",
+        red: "hsl(0, 87%, 67%)",
+        grayishViolet: "hsl(257, 7%, 63%)",
+        veryDarkBlue: "hsl(255, 11%, 22%)",
+        veryDarkViolet: "hsl(260, 8%, 14%) ",
       },
     },
   },
