67e45a62530a9a2e562ba7b23b21341d31a47eb4
Mohd Nizamuddin Mad Yusof <58153279+mohdxnizamv@users.noreply.github.com>
2022-10-13 16:08:04 +0800

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["**/*.html"],
+  darkMode: "class",
   theme: {
     screens: {
       sm: "360px",
@@ -18,6 +19,10 @@ module.exports = {
         cyan: "hsl(180, 66%, 49%)",
         cyanLight: "hsl(180, 66%, 69%)",
         darkViolet: "hsl(257, 27%, 26%)",
+        darkBlue: "hsl(217, 28%, 15%)",
+        darkBlue1: "hsl(218, 28%, 13%)",
+        darkBlue2: "hsl(216, 53%, 9%)",
+        darkBlue3: "hsl(219, 30%, 18%)",
         red: "hsl(0, 87%, 67%)",
         grayishViolet: "hsl(257, 7%, 63%)",
         veryDarkBlue: "hsl(255, 11%, 22%)",
@@ -29,7 +34,16 @@ module.exports = {
           line: "hsl(215, 32%, 27%)",
         },
       },
+      backgroundImage: (theme) => ({
+        "logo-dark-mode": "url('../images/logo-dark-mode.svg')",
+        "logo-light-mode": "url('../images/logo-light-mode.svg')",
+      }),
+    },
+    variants: {
+      extend: {
+        backgroundImage: ["dark"],
+      },
     },
   },
-  darkMode: ["class", '[data-mode="dark"]'],
+  plugins: [],
 };
